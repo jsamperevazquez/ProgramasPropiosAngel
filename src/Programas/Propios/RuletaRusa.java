9074439c302a320d5a462e9f4b3d140ffdec44ae
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2020-11-08 18:28:37 +0100

    Código avanzado, pendiente de un par de arreglos

--- src/Programas/Propios/RuletaRusa.java
+++ src/Programas/Propios/RuletaRusa.java
@@ -1,7 +1,8 @@
 package Programas.Propios;
 
 import javax.swing.*;
-import java.util.Arrays;
+import java.util.ArrayList;
+
 /**
  * @author angel
  * Clase para ver los jugadores que vam a participar el juego
@@ -11,9 +12,10 @@ public class RuletaRusa {
 
     public static void main(String[] args) {
 
+        ArrayList listaPlayers = new ArrayList();
         int numeroJugadores = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el número de jugadores"));
-        String players[];
-        players = new String[10];
+
+
         if (numeroJugadores <= 1) {
             JOptionPane.showMessageDialog(null, "Numero mínimo de jugadores es de 2");
         } else {
@@ -21,42 +23,46 @@ public class RuletaRusa {
             for (int i = 0; i < numeroJugadores; i++) {
                 String nombre = JOptionPane.showInputDialog(null, "Introduce tu nombre jugador" + contador);
                 contador++;
-                players[i] = nombre;
+                listaPlayers.add(nombre);
             }
         }
 
-        JOptionPane.showMessageDialog(null,"EMPIEZA EL JUEGO,  MUCHA SUERTE");
+        JOptionPane.showMessageDialog(null, "EMPIEZA EL JUEGO,  MUCHA SUERTE");
 
         do {
-            int contador2=0;
-            for (int i = 0; i < numeroJugadores; i++) {
+            int contador2 = 0;
+            for (int i = 0; i < listaPlayers.size(); i++) {
 
-                if (players==null){
-                    contador2=0;
-                }
-                JOptionPane.showMessageDialog(null, "Es tu turno " + players[contador2] + " SUERTE");
+                JOptionPane.showMessageDialog(null, "Es tu turno " + listaPlayers.get(contador2) + " SUERTE");
                 int aJugar = Integer.parseInt(JOptionPane.showInputDialog(null, "El tambor está abierto elige del 0 al 5 tu agujero"));
                 while (aJugar < 0 || aJugar > 5) {
                     JOptionPane.showMessageDialog(null, "No seas cagón y mete la bala bien, no ves que estamos en un while");
-                        aJugar = Integer.parseInt(JOptionPane.showInputDialog(null, "El tambor está abierto elige del 0 al 5 tu agujero"));
+                    aJugar = Integer.parseInt(JOptionPane.showInputDialog(null, "El tambor está abierto elige del 0 al 5 tu agujero"));
                 }
 
                 int balazo = (int) (Math.random() * 6);
                 Pistola jugador = new Pistola(aJugar, balazo);
                 jugador.girarTambor(aJugar, balazo);
                 if (aJugar == balazo) {
-                    numeroJugadores = numeroJugadores - 1;
+
+                    listaPlayers.remove(contador2);
+                    if (contador2 == 0) {
+                        contador2 = 1;
+                    }
+                    contador2 = contador2 - 1;
+
                 }
-                if (numeroJugadores == 1) {
-                    JOptionPane.showMessageDialog(null, "El jugador que ha quedado con vida ha ganado");
+                if (listaPlayers.size() == 1) {
+
+                    JOptionPane.showMessageDialog(null, listaPlayers.get(0) + " ha ganado la partida");
                     break;
-                }
-                else
-                contador2 ++;
+                } else
+                    System.out.println(contador2);
+                contador2++;
 
 
             }
         }
-        while (numeroJugadores > 1);
+        while (listaPlayers.size() > 1);
     }
 }
